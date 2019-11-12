287337c12edc79e8c98dd9f5d9ff99e3b2d822dc
Courtney Toller <43355629+courtneytoller@users.noreply.github.com>
2019-11-12 02:25:06 -0700

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(60,MinimumLength=3)]
+        [RegularExpression("^[a-zA-Z0-9 '-]*$")]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Display(Name="Total Earnings")]
+        [Column(TypeName="decimal(18,2")]
+       
+        public decimal Price { get; set; }
+        
+        public string Rating { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("2014-2-26"),
+                        Genre = "Documentary",
+                        Price = 6.0M,
+                        Rating= "PG"
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven ",
+                        ReleaseDate = DateTime.Parse("2002-4-12"),
+                        Genre = "Adventure",
+                        Price = 4.7M,
+                        Rating="PG"
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Ten Commandments",
+                        ReleaseDate = DateTime.Parse("1956-10-5"),
+                        Genre = "Adventure",
+                        Price = 65.5M,
+                        Rating="G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "17 Miracles",
+                        ReleaseDate = DateTime.Parse("2001-6-2"),
+                        Genre = "Adventure",
+                        Price = 2.0M,
+                        Rating="PG"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
